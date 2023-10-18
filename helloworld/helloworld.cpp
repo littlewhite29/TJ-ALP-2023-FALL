94129821c3bd201c475da7309154f96f47ff8109
LW <littlewhite29@qq.com>
2023-10-18 18:08:05 +0800

    10/18 完成本周所有作业

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,29 +1,107 @@
 /* 信管 2051891 黄治东 */
 #include <iostream>
+#include<iomanip>
 using namespace std;
 
 int main()
 {
-	int x;
-
-	while (1) {
-		cout << "请输入x的值[0-100] : ";
-		cin >> x;   //读入x的方式必须是 cin>>int型变量，不允许其他方式
-		if (cin.good() == 0)
+	int y, m, d;
+	int status;//输入状态，取0正常，取1不正常
+	int num = 0;//月份天数
+	int leap = 0;//是否闰年，是取1，否取0
+	while (true)
+	{
+		status = 0;
+		cout << "请输入年份(2000-2030)和月份(1-12) : ";
+		cin >> y >> m;
+		leap = ((y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) ? 1 : 0);
+		if (y < 2000 || y>2030)
 		{
+			status = 1;
+		}
+		if (m <= 0 || m > 12)
+		{
+			status = 1;
+		}
+		//判断输入正确性
+		if (status == 1 || cin.good() == 0)
+		{
+			cout << "输入非法，请重新输入\n";
 			int temp;
-			//cin.ignore(std::numeric_limits< streamsize >::max(), '\n');
 			while ((temp = getchar()) != '\n')
 			{
 				;//空语句
 			}
-			continue;
 		}
-		if (x >= 0 && x <= 100)
+		else
+		{
 			break;
+		}
 	}
-
-	cout << "cin.good()=" << cin.good() << " x=" << x << endl; //此句不准动，并且要求输出时good为1
+	while (true)
+	{
+		status = 0;
+		cout << "请输入" << y << "年" << m << "月1日的星期(0-6表示星期日-星期六) : " << endl;
+		cin >> d;
+		if (d < 0 || d>6)
+		{
+			status = 1;
+		}
+		//判断输入正确性
+		if (status == 1 || cin.good() == 0)
+		{
+			//printf("%d,%d\n",status, ret);
+			cout << "输入非法，请重新输入\n";
+			int temp;
+			while ((temp = getchar()) != '\n')
+			{
+				;//空语句
+			}
+		}
+		else
+		{
+			break;
+		}
+	}
+	if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
+	{
+		num = 31;
+	}
+	else if (m == 2)
+	{
+		num = 28 + leap;
+	}
+	else
+	{
+		num = 30;
+	}
+	//输入正确，开始打印
+	cout << y << "年" << m << "月的月历为:\n";
+	cout << "星期日  星期一  星期二  星期三  星期四  星期五  星期六\n";
+	for (int i = 0; i < d; i++)
+	{
+		//八个空格
+		printf("        ");
+	}
+	int current = 0;
+	while (true)
+	{
+		for (; d < 7; d++)
+		{
+			current++;
+			//四位宽度输出数字加上四个空格
+			cout << setw(4) << current << "    ";
+			if (current == num)
+				break;
+		}
+		d = 0;
+		if (current == num)
+		{
+			cout << endl;
+			break;
+		}
+		cout << endl;
+	}
 
 	return 0;
 }
--- helloworld/homework.c
+++ helloworld/homework.c
@@ -2,30 +2,61 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
+#include<stdbool.h>
 
 int main()
 {
 	int y, m, d;
-	int status = 0;//输入状态，取0正常，取1不正常
+	int status;//输入状态，取0正常，取1不正常
 	int num = 0;//月份天数
 	int leap = 0;//是否闰年，是取1，否取0
-	printf("请输入年份(2000-2030)和月份(1-12) : ");
-	scanf("%d%d", &y, &m);
-	leap = ((y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) ? 1 : 0);
-	if (y < 2000 || y>2030)
+	int ret;
+	while (true)
 	{
-		status = 1;
+		status = 0;
+		printf("请输入年份(2000-2030)和月份(1-12) : ");
+		ret = scanf("%d%d", &y, &m);
+		leap = ((y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) ? 1 : 0);
+		if (y < 2000 || y>2030)
+		{
+			status = 1;
+		}
+		if (m <= 0 || m > 12)
+		{
+			status = 1;
+		}
+		//判断输入正确性
+		if (status == 1 || ret == 0)
+		{
+			printf("输入非法，请重新输入\n");
+			rewind(stdin);
+		}
+		else
+		{
+			break;
+		}
 	}
-	if (m <= 0 || m > 12)
+	while (true)
 	{
-		status = 1;
+		status = 0;
+		printf("请输入%d年%d月1日的星期(0-6表示星期日-星期六) : \n", y, m);
+		ret = scanf("%d", &d);
+		if (d < 0 || d>6)
+		{
+			status = 1;
+		}
+		//判断输入正确性
+		if (status == 1 || ret == 0)
+		{
+			//printf("%d,%d\n",status, ret);
+			printf("输入非法，请重新输入\n");
+			rewind(stdin);
+		}
+		else
+		{
+			break;
+		}
 	}
-	//判断输入正确性
-	if (status == 1)
-	{
-		printf("输入非法，请重新输入\n");
-	}
-
 	if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
 	{
 		num = 31;
@@ -38,7 +69,34 @@ int main()
 	{
 		num = 30;
 	}
-	
 	//输入正确，开始打印
+	printf("%d年%d月的月历为:\n", y, m);
+	printf("星期日  星期一  星期二  星期三  星期四  星期五  星期六\n");
+	for (int i = 0; i < d; i++)
+	{
+		//八个空格
+		printf("        ");
+	}
+	int current = 0;
+	while (true)
+	{
+		for (; d < 7; d++)
+		{
+			current++;
+			//四位宽度输出数字加上四个空格
+			printf("%4d    ", current);
+			if (current == num)
+				break;
+		}
+		d = 0;
+		if (current == num)
+		{
+			printf("\n");
+			break;
+		}
+		printf("\n");
+	}
+
+
 	return 0;
 }
