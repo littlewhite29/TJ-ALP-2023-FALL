d9eaaefac12c78e5e33118911cffa59c22f5df8b
LW <littlewhite29@qq.com>
2023-11-07 15:41:02 +0800

    完成4-part3

--- Debug/compare.bat
+++ Debug/compare.bat
@@ -1,10 +1,10 @@
 ::从示范data文件中取数据
 ::get_input_data test-data.txt 4-b12-02 >data.txt
 
-::w08-s3-demo >demo.txt <data.txt
+::4-b13-demo >demo.txt <data.txt
 ::helloworld >hw.txt <data.txt
 ::txt_compare --file1 demo.txt --file2 hw.txt --display normal
-w08-s2-demo >demo.txt <data.txt
+4-b13-demo >demo.txt <data.txt
 helloworld-c >hw.txt <data.txt
 txt_compare --file1 demo.txt --file2 hw.txt --display normal
 
