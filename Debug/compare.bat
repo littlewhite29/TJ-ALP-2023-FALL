c9a8f6773cc6eb54579b86f4eee550d4df66567d
LW <littlewhite29@qq.com>
2023-11-07 14:15:23 +0800

    更新实验课题目w08-s1~w08-s3

--- Debug/compare.bat
+++ Debug/compare.bat
@@ -1,10 +1,10 @@
 ::从示范data文件中取数据
-get_input_data test-data.txt 4-b12-02 >data.txt
+::get_input_data test-data.txt 4-b12-02 >data.txt
 
-4-b12-demo >demo.txt <data.txt
-helloworld >hw.txt <data.txt
-txt_compare --file1 demo.txt --file2 hw.txt --display normal
-::4-b7-demo >demo.txt <data.txt
-::helloworld-c >hw.txt <data.txt
+::w08-s3-demo >demo.txt <data.txt
+::helloworld >hw.txt <data.txt
 ::txt_compare --file1 demo.txt --file2 hw.txt --display normal
+w08-s2-demo >demo.txt <data.txt
+helloworld-c >hw.txt <data.txt
+txt_compare --file1 demo.txt --file2 hw.txt --display normal
 
--- helloworld-c/4-b6.c
+++ helloworld-c/4-b6.c
@@ -1,4 +1,4 @@
-/////* 2051891 黄治东 信管 */
+///* 2051891 黄治东 信管 */
 //#define _CRT_SECURE_NO_WARNINGS
 //#include <stdio.h>
 //
